9da232e0db8e26546db0ee0ccde660c5bd3a7526
morawaliyadda <sithara.morawaliyadda2000@gmail.com>
2024-02-16 15:18:11 +0530

    dognew

--- animal.py
+++ animal.py
@@ -1,9 +1,16 @@
 import sys
+def dognew():
+    print('BawBaw')
 def default():
     print('Hello')
 
 def main():
-    default()
+    if sys.argv[1]=='dognew':
+        dognew()
+
+    else:
+        default()
+    
 
 if __name__ == '__main__':
     main()
